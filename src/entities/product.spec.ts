bb012eac2808d41df36342f7ccca5ce245315b5b
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2024-05-02 23:56:43 -0300

    feat: add product entity

---
+++ src/entities/product.spec.ts
@@ -0,0 +1,39 @@
+import { Product } from "./product";
+
+describe('Product', () => {
+  it('should be able to create a product', () => {
+    const product = new Product({
+      id: 1,
+      name: 'Product 1',
+      price: 10,
+      stock: 10
+    })
+
+    expect(product.id).toBe(1)
+    expect(product.name).toBe('Product 1')
+    expect(product.price).toBe(10)
+    expect(product.stock).toBe(10)
+  })
+
+  it('should not be able to create a product with invalid price', () => {
+    expect(() => {
+      new Product({
+        id: 1,
+        name: 'Product 1',
+        price: 0,
+        stock: 10
+      })
+    }).toThrow()
+  })
+  
+  it('should not be able to create a product with price greater than 99999999.99', () => {
+    expect(() => {
+      new Product({
+        id: 1,
+        name: 'Product 1',
+        price: 100000000.00,
+        stock: 10
+      })
+    }).toThrow()
+  })
+})
---
+++ src/entities/product.ts
@@ -0,0 +1,44 @@
+type ProductProps = {
+  id: number
+  name: string
+  price: number
+  stock: number
+}
+
+export class Product {
+  private props: ProductProps
+
+  constructor(props: ProductProps) {
+    this.validatePrice(props.price)
+    this.props = props
+  }
+
+  get id() {
+    return this.props.id
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get price() {
+    return this.props.price
+  }
+
+  get stock() {
+    return this.props.stock
+  }
+
+  private validatePrice(price: number) {
+    if (price <= 0) {
+      throw new Error('Price must be greater than zero')
+    }
+
+    const priceString = price.toString()
+    const integerPart = priceString.split('.')[0]
+
+    if (integerPart.length > 8) { // 8 ints + 2 decimals = 10 digits
+      throw new Error('Price must be less than 99999999.99')
+    }
+  }
+}
