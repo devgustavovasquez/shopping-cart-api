ea75061e08f4a148f6fd57abceb5bb0fb90cdf35
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2024-05-03 00:17:02 -0300

    feat: add name value object

---
+++ src/entities/value-objects/name.spec.ts
@@ -0,0 +1,20 @@
+import { Name } from "./name";
+
+describe('Name', () => {
+  it('should be able to create a name', () => {
+    const name = new Name('John Doe')
+    expect(name.value).toBe('John Doe')
+  })
+
+  it('should not be able to create a name with empty value', () => {
+    expect(() => {
+      new Name('')
+    }).toThrow()
+  })
+
+  it('should not be able to create a name with more than 100 characters', () => {
+    expect(() => {
+      new Name('a'.repeat(101))
+    }).toThrow()
+  })
+})
---
+++ src/entities/value-objects/name.ts
@@ -0,0 +1,23 @@
+
+export class Name {
+  private _value: string
+
+  constructor(value: string) {
+    this.validateValue(value)
+    this._value = value
+  }
+
+  get value() {
+    return this._value
+  }
+
+  private validateValue(value: string) {
+    if (value.length <= 0) {
+      throw new Error('Name must be greater than zero characters')
+    }
+
+    if (value.length > 100) {
+      throw new Error('Name must be less than 100 characters')
+    }
+  }
+}
