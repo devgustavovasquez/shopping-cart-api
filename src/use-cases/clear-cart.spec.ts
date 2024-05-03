34b71b69bb3c2dfcbc0efb9bf0c41c3d04712ff1
Gustavo Vasquez <gustavovasquez2002@gmail.com>
2024-05-03 12:46:02 -0300

    feat: add clear cart use case

---
+++ src/use-cases/clear-cart.spec.ts
@@ -0,0 +1,65 @@
+import { ClearCartUseCase } from "./clear-cart";
+import { InMemoryUsersRepository } from "../../tests/repositories/in-memory-users";
+import { InMemoryCartsRepository } from "../../tests/repositories/in-memory-carts";
+import { InMemoryProductsRepository } from "../../tests/repositories/in-memory-products";
+import { makeProduct } from "../../tests/factories/product-factory";
+import { makeUser } from "../../tests/factories/user-factory";
+import { makeCartItem } from "../../tests/factories/cart-item-factory";
+import { makeCart } from "../../tests/factories/cart-factory";
+
+let sut: ClearCartUseCase;
+
+let cartsRepository: InMemoryCartsRepository
+let productsRepository: InMemoryProductsRepository
+let usersRepository: InMemoryUsersRepository 
+
+describe('ClearCart Use Case', () => {
+  beforeEach(() => {
+    cartsRepository = new InMemoryCartsRepository();
+    productsRepository = new InMemoryProductsRepository();
+    usersRepository = new InMemoryUsersRepository();
+    sut = new ClearCartUseCase(cartsRepository, productsRepository, usersRepository);
+  })
+
+  it('should be able to clear a cart', async () => {
+    const user = makeUser();
+    const product = makeProduct({ stock: 50 });
+    const cartItem = makeCartItem({ product, quantity: 5 });
+    const cart = makeCart({ userId: user.id, items: [cartItem] });
+
+    await usersRepository.create(user);
+    await productsRepository.create(product);
+    await cartsRepository.create(cart);
+
+    await sut.execute({ userId: user.id })
+
+    expect(cartsRepository.items[0].items).toHaveLength(0);
+  })
+
+  it("should not be able to clear a cart if user not exists", async () => {
+    const product = makeProduct({ stock: 50 });
+    const cartItem = makeCartItem({ product, quantity: 5 });
+    const cart = makeCart({ userId: 1, items: [cartItem] });
+
+    await productsRepository.create(product);
+    await cartsRepository.create(cart);
+
+    const promise = sut.execute({
+      userId: 2
+    })
+
+    expect(promise).rejects.toThrow();
+  })
+  
+  it("should not be able to clear a cart if cart not exists", async () => {
+    const user = makeUser();
+
+    await usersRepository.create(user);
+
+    const promise = sut.execute({
+      userId: user.id
+    })
+
+    expect(promise).rejects.toThrow();
+  })
+})
---
+++ src/use-cases/clear-cart.ts
@@ -0,0 +1,57 @@
+import { Cart } from '../entities/cart';
+import { CartItem } from '../entities/cart-item';
+import { CartsRepository } from '../repositories/carts-repository';
+import { ProductsRepository } from '../repositories/products-repository';
+import { UsersRepository } from '../repositories/users-repository';
+
+type ClearCartRequest = {
+  userId: number;
+};
+
+type ClearCartResponse = {};
+
+export class ClearCartUseCase {
+  constructor(
+    private readonly cartsRepository: CartsRepository,
+    private readonly productsRepository: ProductsRepository,
+    private readonly usersRepository: UsersRepository
+  ) {}
+
+  async execute(request: ClearCartRequest): Promise<ClearCartResponse> {
+    const { userId } = request;
+
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const cart = await this.cartsRepository.findByUserId(userId);
+
+    if (!cart) {
+      throw new Error('Cart not found');
+    }
+
+    const products = await this.productsRepository.findByIdRange(
+      cart.items.map((item) => item.product.id),
+    )
+
+    cart.items.forEach(async (item) => {
+      const product = products.find((product) => product.id === item.product.id);
+
+      if (!product) {
+        return;
+      }
+
+      // TODO: improve this
+      product.stock = product.stock + item.quantity;
+      await this.productsRepository.save(product);
+    })
+
+    cart.clearCart();
+
+    await this.cartsRepository.save(cart);
+
+    return {};
+  }
+}
